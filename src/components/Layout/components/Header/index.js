853cb88361aef48b38f6e71760da89f55cc035e7
vientran199 <vientran1999@gmail.com>
2022-05-30 00:47:00 +0700

    create base layout

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,5 +1,16 @@
+import classNames from 'classnames/bind'
+import styles from './Header.module.scss'
+
+const cx = classNames.bind(styles) // Khong dùng classNames, thì sẽ dùng: classname={styles.postItem}, classname={style['post-item']}
+//Khi dùng classname.bind : cx('post-item')
+
 function Header() {
-    return <h2>Header</h2>;
+    return <header className={cx('wrapper')}>
+        <div className={cx('inner')}>
+            {/* logo */}
+            {/* search */}
+        </div>
+    </header>;
 }
 
 export default Header;
