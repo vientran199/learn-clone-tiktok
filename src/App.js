64e625184d6b3f4d2a0a9997a4845b0079621f92
vientran199 <vientran1999@gmail.com>
2022-05-30 00:00:45 +0700

    config Router/Layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { publicRoutes } from '~/routes'
+import { DefaultLayout } from '~/components/Layout';
 function App() {
-  return (
-    <div className="App">
-      <h1>Reatc app</h1>
-    </div>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <Routes>
+                    {publicRoutes.map((route, index) => {
+                        const Layout = route.layout ? route.layout : DefaultLayout
+                        const Page = route.component
+                        return (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <Page />
+                                    </Layout>
+                                }
+                            />
+                        )
+                    }
+                    )}
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
